9a9df4a053709a747e007d8a241fcfda4a1e71fb
hoatrana3 <vietnamvodichhsts@gmail.com>
2020-06-10 10:34:20 +0700

    improve

--- scripts/components/museum-image.js
+++ scripts/components/museum-image.js
@@ -10,9 +10,8 @@ AFRAME.registerComponent('museum-image', {
         }
     },
     init: function () {
-        const viewer = viewers[this.data.viewer]
-
         this.el.addEventListener('click', () => {
+            const viewer = viewers[this.data.viewer]
             viewer.show()
             if (this.data.index !== -1) viewer.view(this.data.index)
         })
